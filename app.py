14d72f5dc2daad67abf21213b5bb8e254a372601
Owen Sullivan <multiojuice@gmail.com>
2019-02-03 15:50:01 -0500

    Flask graphene custom hello world

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import graphene
+from flask_graphql import GraphQLView
+from flask import Flask
+
+# app initialization
+app = Flask(__name__)
+app.debug = True
 
 class Query(graphene.ObjectType):
     hello = graphene.String(description='A typical hello world')
@@ -7,3 +13,19 @@ class Query(graphene.ObjectType):
         return 'World'
 
 schema = graphene.Schema(query=Query)
+
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True # for having the GraphiQL interface
+    )
+)
+
+@app.route('/')
+def index():
+    return '<p> Hello World</p>'
+
+if __name__ == '__main__':
+    app.run()
